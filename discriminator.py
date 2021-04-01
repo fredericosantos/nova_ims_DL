f9f0386cc9569601be57ab1a3215381537741dd9
iv-kis <i.kisialio@gmail.com>
2021-04-01 02:09:45 +0100

    Discriminator with max pool and 3 conv blocks

--- discriminator.py
+++ discriminator.py
@@ -10,7 +10,7 @@ import tensorflow as tf
 import tensorflow.keras as keras
 
 from tensorflow_addons.layers import InstanceNormalization
-from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, MaxPool2D
 import os
 import time
 from utils import ReflectionPadding2D, initializer
@@ -20,9 +20,7 @@ def Discriminator(
     filters: int = 64,
     input_shape: int = 256,
     input_filters: int = 3,
-    n_convBlocks: int = 2,
-    n_resBlocks: int = 6,
-    #n_deconvBlocks: int = 2,
+    n_convBlocks: int = 3,
     name=None,
 ):
     """Discriminator for CycleGAN
@@ -36,7 +34,7 @@ def Discriminator(
     x = ReflectionPadding2D(padding=(3, 3), name="REF_0")(img_input)
 
     # First 7x7, convolution c7s1_64
-    x = convBlock(filters=filters, activation="relu", strides=1, 
+    x = convBlock(filters=filters, activation="relu", strides=1,
                   kernel_size=7, norm_type="instance", padding="valid",
                   name=f"c7s1-{filters}")(x)
 
@@ -45,22 +43,22 @@ def Discriminator(
         filters *= 2
         x = convBlock(filters=filters, activation="relu",
                       norm_type="instance", name=f"d{filters}")(x)
-
-    # Residual Blocks
-    for _ in range(n_resBlocks):
-        x = resBlock(x, activation="relu", norm_type="instance",
-                     name=f"R{x.shape[-1]}_block{_}")
+        # MaxPooling after every 3 convolution blocks
+        if ( _ % 3 == 1):
+            x = MaxPool2D((2, 2), strides=2)(x)
 
     # Global Average Pooling
     x = GlobalAveragePooling2D()(x)
 
-    # Output 
+    # Output
     x = Dense(1, activation="sigmoid")(x)
 
     model = keras.models.Model(img_input, x, name=name)
     return model
 
-# generator = Generator()
-# tf.keras.utils.plot_model(generator, to_file="generator.png", show_shapes=True, dpi=128)
+discriminator = Discriminator()
 
+tf.keras.utils.plot_model(discriminator, to_file="discriminator.png", show_shapes=True, dpi=128)
+with open('discriminator.txt', 'w') as f:
+    discriminator.summary(print_fn=lambda x: f.write(x + '\n'))
 # %%
