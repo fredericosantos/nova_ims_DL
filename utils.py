817afc81d7fc53c720495389c94af434164612c8
Frederico Santos <1119791+fredericosantos@users.noreply.github.com>
2021-04-01 22:45:06 +0100

    updating utils
    backward compatibility and keeping all changes

--- utils.py
+++ utils.py
@@ -5,18 +5,18 @@ import os
 IMG_HEIGHT = 64
 IMG_WIDTH = IMG_HEIGHT
 N_CHANNELS = 3
-batch_size = 1
+BATCH_SIZE = 1
 
 # generates a keras.dataset object for train or val.
-def train_ds_gen(dir_name,val_split=0.2,val=False,batch_size=1):
+def train_ds_gen(dir_name, val_split=0.2, val=False, batch_size=1):
     """ Generates a dataset. 
     Returns a tf.data.Dataset object.
     Parameters: directory: the folder where the images are.
     val_split = number to feed to keras.preprocess.image from directory. Default is 0.2.
     val = whether the func generates a training or val dataset. Default generates train.
     """
-    directory = os.path.join(os.getcwd(),"datasets",dir_name)
-    
+    directory = os.path.join(os.getcwd(), "datasets", dir_name)
+
     if val:
         subset = "validation"
     else:
@@ -27,9 +27,9 @@ def train_ds_gen(dir_name,val_split=0.2,val=False,batch_size=1):
         validation_split=val_split,
         subset=subset,
         seed=42,
-        batch_size=batch_size
-        )
-    
+        batch_size=BATCH_SIZE,
+    )
+
     return train_ds
 
 
@@ -40,13 +40,65 @@ def normalize_img(img):
     # Map values in the range [-1, 1]
     return (img / 127.5) - 1.0
 
-def preprocess_train_img(tensor_img,tensor_label):    
-    tensor_img = tf.image.resize(tensor_img, [150,150])
-    tensor_img = tf.image.random_crop(tensor_img,[batch_size,128,128,3])
-    tensor_img = tf.image.random_flip_left_right(tensor_img)
-    #tensor_img = normalize_img(img)
-    return tensor_img,tensor_label
 
+def preprocess_train_img(tensor_img, tensor_label):
+    tensor_img = tf.image.resize(tensor_img, [150, 150])
+    tensor_img = tf.image.random_crop(tensor_img, [batch_size, 128, 128, 3])
+    tensor_img = tf.image.random_flip_left_right(tensor_img)
+    # tensor_img = normalize_img(img)
+    return tensor_img, tensor_label
+
+
+# Keeping backward compatibility
+# randomly crop the image
+def random_crop(image):
+    return tf.image.random_crop(image, size=[IMG_HEIGHT, IMG_WIDTH, N_CHANNELS])
+
+
+def normalize(image):
+    image = tf.cast(image, tf.float32)
+    image /= 127.5
+    return image - 1
+
+
+def image_resize(image):
+    image = tf.image.resize(
+        image,
+        [int(IMG_HEIGHT), int(IMG_WIDTH)],
+        method=tf.image.ResizeMethod.NEAREST_NEIGHBOR,
+    )
+    return image
+
+
+def random_jitter(image, resize_zoom: float = 1.1):
+    """
+    resize_zoom => Increase the image size (that will go into the network)
+    by resize_zoom (eg. 255 * 1.1) before randomly cropping it by the image size (eg. 255x255).
+    """
+
+    # Resize image if we're choosing to resize
+    image = tf.image.resize(
+        image,
+        [int(IMG_HEIGHT * resize_zoom), int(IMG_WIDTH * resize_zoom)],
+        method=tf.image.ResizeMethod.NEAREST_NEIGHBOR,
+    )
+
+    # Crop the image with the given height and width
+    image = random_crop(image)
+    image = tf.image.random_flip_left_right(image)
+    return image
+
+
+def preprocess_image_train(image, label):
+
+    image = random_jitter(image)
+    image = normalize(image)
+    return image
+
+
+def preprocess_image_test(image, label):
+    image = normalize(image_resize(image))
+    return image
 
 
 ## Padding
