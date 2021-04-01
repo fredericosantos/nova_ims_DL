4b60db8a9d46a1ac797bd1c601b277c7861d9814
Exileus <71491915+Exileus@users.noreply.github.com>
2021-04-01 12:44:13 +0100

    Update utils.py
    Now generates datasets. Now can also preprocess said datasets. Still need to figure out a more optimal resize and crop parameters. Still need to figure out what and how normalize is doing this because it's probably normalizing more of the tensor than what it's supposed to.

--- utils.py
+++ utils.py
@@ -5,9 +5,10 @@ import os
 IMG_HEIGHT = 128
 IMG_WIDTH = 128
 N_CHANNELS = 3
+batch_size = 1
 
 # generates a keras.dataset object for train or val.
-def train_ds_gen(dir_name,val_split=0.2,val=False):
+def train_ds_gen(dir_name,val_split=0.2,val=False,batch_size=1):
     """ Generates a dataset. 
     Returns a tf.data.Dataset object.
     Parameters: directory: the folder where the images are.
@@ -15,6 +16,7 @@ def train_ds_gen(dir_name,val_split=0.2,val=False):
     val = whether the func generates a training or val dataset. Default generates train.
     """
     directory = os.path.join(os.getcwd(),"datasets",dir_name)
+    
     if val:
         subset="validation"
     else:
@@ -24,42 +26,30 @@ def train_ds_gen(dir_name,val_split=0.2,val=False):
         directory=directory,
         validation_split=val_split,
         subset=subset,
-        seed=42
+        seed=42,
+        batch_size=batch_size
         )
     
     return train_ds
 
-# randomly crop the image
-def random_crop(image):
-    return tf.image.random_crop(image, size=[IMG_HEIGHT, IMG_WIDTH, N_CHANNELS])
 
-def normalize(image):
-    image = tf.cast(image, tf.float32)
-    image /= 127.5
-    return image - 1
+## Preprocess
+## Normalize still isn't working, need to understand what, how and why
+def normalize_img(img):
+    img = tf.cast(img, dtype=tf.float32)
+    # Map values in the range [-1, 1]
+    return (img / 127.5) - 1.0
 
-def random_jitter(image, resize_zoom: float = 1.1):
-    """
-    resize_zoom => Increase the image size (that will go into the network)
-    by resize_zoom (eg. 255 * 1.1) before randomly cropping it by the image size (eg. 255x255).
-    """
+def preprocess_train_img(tensor_img,tensor_label):    
+    tensor_img = tf.image.resize(tensor_img, [150,150])
+    tensor_img = tf.image.random_crop(tensor_img,[batch_size,128,128,3])
+    tensor_img = tf.image.random_flip_left_right(tensor_img)
+    #tensor_img = normalize_img(img)
+    return tensor_img,tensor_label
 
-    # Resize image if we're choosing to resize
-    image = tf.image.resize(image, [int(IMG_HEIGHT*resize_zoom), int(
-        IMG_WIDTH*resize_zoom)], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
 
-    # Crop the image with the given height and width
-    image = random_crop(image)
-    image = tf.image.random_flip_left_right(image)
-    return image
 
-def preprocess_image(image, label, phase: "train/test"):
-    if phase == "train":
-        image = random_jitter(image)
-    if phase == "test":
-        image = tf.image.resize(image,[IMG_HEIGHT,IMG_WIDTH],method=tf.imageResizeMethod.NEAREST_NEIGHBOR)
-    return normalize(image)
-
+## Padding
 class ReflectionPadding2D(tf.keras.layers.Layer):
     """Reflective padding used in the paper.
     https://arxiv.org/pdf/1703.10593.pdf ==> 7.2 Network architectures. 
